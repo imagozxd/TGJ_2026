05e841d2f661bf5a82ae27a173c191728848badf
Dreamer_P6 <dreamerp6ac@gmail.com>
2026-03-29 17:31:17 -0500

    Añadida un monton de cosas XD

--- Assets/Scripts/barra clientes/ClienteMover.cs
+++ Assets/Scripts/barra clientes/ClienteMover.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClienteMover : MonoBehaviour
 {
+
+    [SerializeField] private Image indicatorIMG;
+    [SerializeField] private Sprite susyDiazFaceSPR;
+    [SerializeField] private Sprite dinaFaceSPR;
+    [SerializeField] private Sprite zumbaFaceSPR;
+
     public float speed;
     private Vector3 target;
 
     public bool enLocal = false;
+    Cliente cliente;
 
-    public void Init(Vector3 targetPos, float moveSpeed)
+    public void Init(Vector3 targetPos, float moveSpeed, Cliente newCliente)
     {
         target = targetPos;
         speed = moveSpeed;
+        cliente = newCliente;
+
+        if (cliente.tipo == TipoCliente.SusyDiaz)
+        {
+            indicatorIMG.sprite = susyDiazFaceSPR;
+        }
+        else if (cliente.tipo == TipoCliente.Dina)
+        {
+            indicatorIMG.sprite = dinaFaceSPR;
+        }
+        else if (cliente.tipo == TipoCliente.Zumba)
+        {
+            indicatorIMG.sprite = zumbaFaceSPR;
+        }
     }
 
     void Update()
@@ -23,11 +45,16 @@ public class ClienteMover : MonoBehaviour
             speed * Time.deltaTime
         );
 
-        // llegó a B
+        // llegï¿½ a B
         if (Vector3.Distance(transform.position, target) < 1f)
         {
             enLocal = true;
             ClienteBarControllerSimple.Instance.ClienteLlego(this);
         }
+    }
+
+    public Cliente GetCliente()
+    {
+        return cliente;
     }
 }
